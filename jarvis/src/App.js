408bdfe79f04532aa2fc2a5eb59c3d65132faf33
Negan <alexvb1@protonmail.com>
2023-01-10 20:43:09 +0200

    testing

--- jarvis/src/App.js
+++ jarvis/src/App.js
@@ -7,16 +7,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Speak the text
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* add an input box */}
+        <input type="text" id="text" />
+        
+        <button id="speak">Speak</button>
       </header>
     </div>
   );
